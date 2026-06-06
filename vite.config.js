1aabd786c04555935371796e628c8fcc60f00c77
Omar Aldayr García Hernández <aldayr935@gmail.com>
2026-06-05 18:52:42 -0600

    Screenshots PWA agregados

--- vite.config.js
+++ vite.config.js
@@ -8,28 +8,39 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'To-Do App',
-        short_name: 'To-Do',
-        description: 'App para gestionar tus listas',
-        theme_color: '#1d4ed8',
-        background_color: '#f3f4f6',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+  registerType: 'autoUpdate',
+  manifest: {
+    name: 'To-Do App',
+    short_name: 'To-Do',
+    description: 'App para gestionar tus listas',
+    theme_color: '#1d4ed8',
+    background_color: '#f3f4f6',
+    display: 'standalone',
+    start_url: '/',
+    icons: [
+      {
+        src: '/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png'
       }
-    })
-  ],
+    ],
+    screenshots: [
+      {
+        src: '/screenshot-mobile.png',
+        sizes: '390x844',
+        type: 'image/png'
+      },
+      {
+        src: '/screenshot-desktop.png',
+        sizes: '1280x800',
+        type: 'image/png'
+        form_factor: 'wide'
+      }
+    ]
+  }
 })
